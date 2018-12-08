a9979ed7a14c963398909614634c4eae7370d71d
Guilherme Barbosa Pereira <guilherme.barbosapereira@outlook.com>
2018-12-08 15:03:52 -0200

    Changed language from pt-BR to en and updated screenshots

--- lib/network/movie_network.dart
+++ lib/network/movie_network.dart
@@ -5,7 +5,7 @@ import 'package:cinema/constants.dart';
 
 abstract class MovieNetwork {
   static Future<dynamic> listPopularMovie() async {
-    String url = 'https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=pt-BR';
+    String url = 'https://api.themoviedb.org/3/movie/popular?api_key=$apiKey';
     var res = await http.get(url, headers: { 'Accept': 'application/json' }).catchError((error) {
       print('error: ${error.toString()}');
     });
@@ -13,7 +13,7 @@ abstract class MovieNetwork {
   }
 
   static Future<Map> getMovieDetails(int movieId) async {
-    String url = 'https://api.themoviedb.org/3/movie/$movieId?api_key=$apiKey&language=pt-BR&append_to_response=casts&include_image_language=pt-BR,en';
+    String url = 'https://api.themoviedb.org/3/movie/$movieId?api_key=$apiKey&append_to_response=casts';
 
     var res = await http.get(url, headers: {
       'Accept': 'application/json'
